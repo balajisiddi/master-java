71b1f1a3a5019157feeb974716293c549fa82479
Balaji Siddi <balaji.siddi@gmail.com>
2025-12-03 14:24:46 +0530

    streams added

--- Main/Streams.java
+++ Main/Streams.java
@@ -1,15 +1,18 @@
 package Main;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Streams {
+    private static long counter = 0;
+    private static void wasCalled() {
+        counter++;
+    }
     public static void main(String[] args) {
 
         Stream<String> myStream = Stream.empty();
@@ -18,6 +21,19 @@ public class Streams {
 
         Stream<String> streamOfStrings = Pattern.compile(", ")
         .splitAsStream("a, b, c");
+            
+        Optional<String> firstElement= streamOfStrings.findFirst();
 
+        Stream<String> stream = Stream.of("a", "b", "c").filter(s-> s.contains("b"));
+
+        Optional<String> anyElement = stream.findAny();
+
+        Stream.of(1,2,3,4,5).skip(1).map(n -> n +1).forEach(System.out::println);
+
+        List<String> list = Arrays.asList("a", "b", "c");
+        list.stream().filter(s-> {
+            wasCalled();
+            return s.contains("b");
+        });
     }
 }
